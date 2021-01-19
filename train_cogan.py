Fix scalar summaries to use the epoch's average

train_cogan.py
@@ -88,12 +88,12 @@ disc_print.train()
 #     print(i, p.size())
 # exit()
 
-optimizer_G = torch.optim.Adam(
+optimizer_g = torch.optim.Adam(
     params=list(net_photo.parameters()) + list(net_print.parameters()),
     lr=1e-4,
     weight_decay=1e-4
 )
-optimizer_D = torch.optim.Adam(
+optimizer_d = torch.optim.Adam(
     params=list(disc_photo.parameters()) + list(disc_print.parameters()),
     lr=1e-4,
     weight_decay=1e-4
@@ -111,13 +111,27 @@ print(len(train_loader))
 Tensor = torch.cuda.FloatTensor
 patch = (1, 256 // 2 ** 4, 256 // 2 ** 4)
 
+g_loss_meter = AverageMeter()
+g_id_loss_meter = AverageMeter()
+g_adv_photo_loss_meter = AverageMeter()
+g_adv_print_loss_meter = AverageMeter()
+g_adv_loss_raw_meter = AverageMeter()
+g_adv_loss_meter = AverageMeter()
+g_l2_photo_loss_meter = AverageMeter()
+g_l2_print_loss_meter = AverageMeter()
+g_l2_loss_raw_meter = AverageMeter()
+g_l2_loss_meter = AverageMeter()
+
+d_loss_meter = AverageMeter()
+d_real_photo_loss_meter = AverageMeter()
+d_real_print_loss_meter = AverageMeter()
+d_fake_photo_loss_meter = AverageMeter()
+d_fake_print_loss_meter = AverageMeter()
+
+accuracy_meter = AverageMeter()
+
 for epoch in range(500):
     print(epoch)
-
-    loss_m_d = AverageMeter()
-    loss_m_g = AverageMeter()
-    acc_m = AverageMeter()
-
     for step, (img_photo, img_print, lbl) in enumerate(train_loader):
         # plot_tensor([img_photo[0], img_print[0]])
         bs = img_photo.size(0)
@@ -140,43 +154,55 @@ for epoch in range(500):
         pred_fake_photo = disc_photo(fake_photo)
         pred_fake_print = disc_print(fake_print)
 
-        gen_adversarial_photo_loss = adversarial_photo_loss(pred_fake_photo,
-                                                            valid.to(gpu0)).to(cpu)
-        gen_adversarial_print_loss = adversarial_print_loss(pred_fake_print,
-                                                            valid.to(gpu1)).to(cpu)
+        g_adv_photo_loss = adversarial_photo_loss(pred_fake_photo,
+                                                  valid.to(gpu0)).to(cpu)
+        g_adv_print_loss = adversarial_print_loss(pred_fake_print,
+                                                  valid.to(gpu1)).to(cpu)
+        g_adv_photo_loss_meter.update(g_adv_photo_loss.item())
+        g_adv_print_loss_meter.update(g_adv_print_loss.item())
 
-        gen_l2_photo_loss = l2_photo_loss(fake_photo, img_photo).to(cpu)
-        gen_l2_print_loss = l2_print_loss(fake_print, img_print).to(cpu)
-
-        adversarial_loss = (
-            gen_adversarial_photo_loss +
-            gen_adversarial_print_loss
+        g_adv_loss_raw = (
+            g_adv_photo_loss +
+            g_adv_print_loss
         ) / 2
+        g_adv_loss_raw_meter.update(g_adv_loss_raw.item())
 
-        l2_loss = (
-            gen_l2_photo_loss +
-            gen_l2_print_loss
+        g_adv_loss = g_adv_loss_raw * args.delta_1
+        g_adv_loss_meter.update(g_adv_loss.item())
+
+        g_l2_photo_loss = l2_photo_loss(fake_photo, img_photo).to(cpu)
+        g_l2_print_loss = l2_print_loss(fake_print, img_print).to(cpu)
+
+        g_l2_photo_loss_meter.update(g_l2_photo_loss.item())
+        g_l2_print_loss_meter.update(g_l2_print_loss.item())
+
+        g_l2_loss_raw = (
+            g_l2_photo_loss +
+            g_l2_print_loss
         ) / 2
+        g_l2_loss_raw_meter.update(g_l2_loss_raw.item())
+
+        g_l2_loss = g_l2_loss_raw * args.delta_2
+        g_l2_loss_meter.update(g_l2_loss.item())
 
         dist = ((y_photo.to(cpu) - y_print.to(cpu)) ** 2).sum(1)
 
         margin = torch.ones_like(dist, device=cpu) * args.margin
 
-        identity_loss = (lbl * dist + (1 - lbl) * F.relu(margin - dist)).mean()
-        loss = (identity_loss +
-                adversarial_loss * args.delta_1 +
-                l2_loss * args.delta_2)
+        g_id_loss = (lbl * dist + (1 - lbl) * F.relu(margin - dist)).mean()
+        g_id_loss_meter.update(g_id_loss.item())
 
-        optimizer_G.zero_grad()
-        loss.backward()
-        optimizer_G.step()
+        g_loss = g_id_loss + g_adv_loss + g_l2_loss
+        g_loss_meter.update(g_loss.item())
+
+        optimizer_g.zero_grad()
+        g_loss.backward()
+        optimizer_g.step()
 
         acc = (dist < args.margin).type(torch.float)
         acc = (acc == lbl).type(torch.float)
         acc = acc.mean()
-        acc_m.update(acc)
-
-        loss_m_g.update(loss.item())
+        accuracy_meter.update(acc)
 
         # """"""""""""""""""
         # " Discriminator  "
@@ -187,81 +213,111 @@ for epoch in range(500):
         pred_real_print = disc_print(img_print)
         pred_fake_print = disc_print(fake_print.detach())
 
-        discriminator_real_photo_loss = adversarial_photo_loss(pred_real_photo,
-                                                               valid.to(gpu0)).to(cpu)
-        discriminator_real_print_loss = adversarial_print_loss(pred_real_print,
-                                                               valid.to(gpu1)).to(cpu)
-        discriminator_fake_photo_loss = adversarial_photo_loss(pred_fake_photo,
-                                                               fake.to(gpu0)).to(cpu)
-        discriminator_fake_print_loss = adversarial_print_loss(pred_fake_print,
-                                                               fake.to(gpu1)).to(cpu)
+        d_real_photo_loss = adversarial_photo_loss(pred_real_photo, valid.to(gpu0)).to(cpu)
+        d_real_print_loss = adversarial_print_loss(pred_real_print, valid.to(gpu1)).to(cpu)
+        d_fake_photo_loss = adversarial_photo_loss(pred_fake_photo, fake.to(gpu0)).to(cpu)
+        d_fake_print_loss = adversarial_print_loss(pred_fake_print, fake.to(gpu1)).to(cpu)
+
+        d_real_photo_loss_meter.update(d_real_photo_loss.item())
+        d_real_print_loss_meter.update(d_real_print_loss.item())
+        d_fake_photo_loss_meter.update(d_fake_photo_loss.item())
+        d_fake_print_loss_meter.update(d_fake_print_loss.item())
 
         d_loss = (
-            discriminator_real_photo_loss +
-            discriminator_real_print_loss +
-            discriminator_fake_photo_loss +
-            discriminator_fake_print_loss
+            d_real_photo_loss +
+            d_real_print_loss +
+            d_fake_photo_loss +
+            d_fake_print_loss
         ) / 4
+        d_loss_meter.update(d_loss.item())
 
-        optimizer_D.zero_grad()
+        optimizer_d.zero_grad()
         d_loss.backward()
-        optimizer_D.step()
-
-        loss_m_d.update(d_loss.item())
+        optimizer_d.step()
 
         if step % 10 == 0:
             print(
                 'Epoch: %02d, iter: %02d/%02d, D loss: %.4f, G loss: %.4f, acc: %.4f'
-                % (epoch, step, len(train_loader), loss_m_d.avg, loss_m_g.avg, acc_m.avg)
+                % (epoch, step, len(train_loader),
+                   d_loss_meter.avg,
+                   g_loss_meter.avg,
+                   accuracy_meter.avg)
             )
-        if step % 100 == 0:
-            writer.add_scalar('loss/generator/adversarial_photo',
-                              gen_adversarial_photo_loss,
-                              step)
-            writer.add_scalar('loss/generator/adversarial_print',
-                              gen_adversarial_print_loss,
-                              step)
-            writer.add_scalar('loss/generator/adversarial',
-                              adversarial_loss,
-                              step)
-            writer.add_scalar('loss/generator/l2_photo', gen_l2_photo_loss, step)
-            writer.add_scalar('loss/generator/l2_print', gen_l2_print_loss, step)
-            writer.add_scalar('loss/generator/l2', l2_loss, step)
-            writer.add_scalar('loss/generator/identity', identity_loss, step)
-            writer.add_scalar('loss/generator/total', loss, step)
-            writer.add_scalar('loss/discriminator/real_photo',
-                              discriminator_real_photo_loss,
-                              step)
-            writer.add_scalar('loss/discriminator/real_print',
-                              discriminator_real_print_loss,
-                              step)
-            writer.add_scalar('loss/discriminator/fake_photo',
-                              discriminator_fake_photo_loss,
-                              step)
-            writer.add_scalar('loss/discriminator/fake_print',
-                              discriminator_fake_print_loss,
-                              step)
-            writer.add_scalar('loss/discriminator/total', d_loss, step)
-            writer.add_scalar('accuracy', acc, step)
+        if (step + 1) % len(train_loader) == 0:
+            global_step = epoch * len(train_loader) + step + 1
+            writer.add_scalar('generator/adversarial_photo',
+                              g_adv_photo_loss_meter.avg,
+                              global_step)
+            writer.add_scalar('generator/adversarial_print',
+                              g_adv_print_loss_meter.avg,
+                              global_step)
+            writer.add_scalar('generator/adversarial_raw',
+                              g_adv_loss_raw_meter.avg,
+                              global_step)
+            writer.add_scalar('generator/adversarial',
+                              g_adv_loss_meter.avg,
+                              global_step)
+            writer.add_scalar('generator/l2_photo', g_l2_photo_loss_meter.avg, global_step)
+            writer.add_scalar('generator/l2_print', g_l2_print_loss_meter.avg, global_step)
+            writer.add_scalar('generator/l2_raw', g_l2_loss_raw_meter.avg, global_step)
+            writer.add_scalar('generator/l2', g_l2_loss_meter.avg, global_step)
+            writer.add_scalar('generator/identity', g_id_loss_meter.avg, global_step)
+            writer.add_scalar('loss/generator', g_loss_meter.avg, global_step)
+
+            writer.add_scalar('discriminator/real_photo',
+                              d_real_photo_loss_meter.avg,
+                              global_step)
+            writer.add_scalar('discriminator/real_print',
+                              d_real_print_loss_meter.avg,
+                              global_step)
+            writer.add_scalar('discriminator/fake_photo',
+                              d_fake_photo_loss_meter.avg,
+                              global_step)
+            writer.add_scalar('discriminator/fake_print',
+                              d_fake_print_loss_meter.avg,
+                              global_step)
+            writer.add_scalar('loss/discriminator', d_loss_meter.avg, global_step)
+
+            writer.add_scalar('accuracy', acc, global_step)
 
             real_photo_grid = torchvision.utils.make_grid(img_photo[:25], nrow=5)
             real_print_grid = torchvision.utils.make_grid(img_print[:25], nrow=5)
             fake_photo_grid = torchvision.utils.make_grid(fake_photo[:25], nrow=5)
             fake_print_grid = torchvision.utils.make_grid(fake_print[:25], nrow=5)
 
-            writer.add_image('real photo', real_photo_grid, step)
-            writer.add_image('real print', real_print_grid, step)
-            writer.add_image('fake photo', fake_photo_grid, step)
-            writer.add_image('fake print', fake_print_grid, step)
+            writer.add_image('real photo', real_photo_grid, global_step)
+            writer.add_image('real print', real_print_grid, global_step)
+            writer.add_image('fake photo', fake_photo_grid, global_step)
+            writer.add_image('fake print', fake_print_grid, global_step)
+            writer.flush()
+
+            g_loss_meter.reset()
+            g_id_loss_meter.reset()
+            g_adv_photo_loss_meter.reset()
+            g_adv_print_loss_meter.reset()
+            g_adv_loss_raw_meter.reset()
+            g_adv_loss_meter.reset()
+            g_l2_photo_loss_meter.reset()
+            g_l2_print_loss_meter.reset()
+            g_l2_loss_raw_meter.reset()
+            g_l2_loss_meter.reset()
+            d_loss_meter.reset()
+            d_real_photo_loss_meter.reset()
+            d_real_print_loss_meter.reset()
+            d_fake_photo_loss_meter.reset()
+            d_fake_print_loss_meter.reset()
+            accuracy_meter.reset()
 
     state = {
         'net_photo': net_photo.state_dict(),
         'net_print': net_print.state_dict(),
         'disc_photo': disc_photo.state_dict(),
         'disc_print': disc_print.state_dict(),
-        'optimizer': optimizer_G.state_dict()
+        'optimizer': optimizer_g.state_dict()
     }
 
     ckpt_fp = os.path.join(ckpt_dir, 'checkpoint.pt')
     torch.save(state, ckpt_fp)
     print('\nModel saved!\n')
+
+writer.close()