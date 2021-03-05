f8ba6e1342255d8a589b1c79a3cf1d97497dcd44
Domenick Poster <dposter@mix.wvu.edu>
2021-03-04 23:37:01 -0500

    Started WVU dataset script

---
+++ wvu.py
@@ -0,0 +1,18 @@
+import os
+import math
+import shutil
+import numpy as np
+import random as python_random
+from skimage.io import imread
+from sklearn.model_selection import KFold
+
+
+VIS_DIR = '/home/hulk1/data/periocular/WVU_Face_Iris_database/FACE PERIOCULAR'
+NIR_DIR = '/home/hulk1/data/periocular/WVU_Face_Iris_database/IRIS and Face'
+
+
+def create_open_world_protocol():
+    pass
+
+if __name__ == '__main__':
+    create_open_world_protocol()
