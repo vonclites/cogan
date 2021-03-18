b29018cfcf5663cb976edb9c44795e2db62b345d
Domenick Poster <dposter@mix.wvu.edu>
2021-03-18 12:37:31 -0400

    Removing default arg parse values

--- train_cogan.py
+++ train_cogan.py
@@ -25,20 +25,18 @@ Tensor = torch.cuda.FloatTensor
 
 def parse_args():
     parser = argparse.ArgumentParser()
-    parser.add_argument('--batch_size', default=128, type=int, help='batch size')
-    parser.add_argument('--num_epochs', default=400, type=int, help='batch size')
-    parser.add_argument('--margin', default=100, type=int, help='batch size')
-    parser.add_argument('--adversarial_coeff', default=1, type=float,
+    parser.add_argument('--batch_size', type=int, help='batch size')
+    parser.add_argument('--num_epochs', type=int, help='batch size')
+    parser.add_argument('--margin', type=int, help='batch size')
+    parser.add_argument('--adversarial_coeff', type=float,
                         help='Adversarial Loss Coefficient')
-    parser.add_argument('--pixel_coeff', default=1, type=float,
+    parser.add_argument('--pixel_coeff', type=float,
                         help='Pixel-to-Pixel Loss Coefficient')
-    parser.add_argument('--perceptual_coeff', default=1, type=float,
+    parser.add_argument('--perceptual_coeff', type=float,
                         help='Perceptual Loss Coefficient')
     parser.add_argument('--nir_dir', type=str,
-                        default='/home/hulk1/data/periocular/hk/images/dev/NIR',
                         help='path to data')
     parser.add_argument('--vis_dir', type=str,
-                        default='/home/hulk1/data/periocular/hk/images/dev/VIS',
                         help='path to data')
     parser.add_argument('--valid_train_classes_fp', type=str,
                         help='text file of class labels to include in training dataset')
@@ -47,21 +45,17 @@ def parse_args():
     parser.add_argument('--model_dir', type=str,
                         help='base directory path in which individual runs will be saved')
     parser.add_argument('--nir_mean_fp', type=str,
-                        default='/home/hulk1/data/periocular/hk/stats/nir_mean.txt',
                         help='Path to file containing channel-wise image statistic')
     parser.add_argument('--nir_std_fp', type=str,
-                        default='/home/hulk1/data/periocular/hk/stats/nir_std.txt',
                         help='Path to file containing channel-wise image statistic')
     parser.add_argument('--vis_mean_fp', type=str,
-                        default='/home/hulk1/data/periocular/hk/stats/vis_mean.txt',
                         help='Path to file containing channel-wise image statistic')
     parser.add_argument('--vis_std_fp', type=str,
-                        default='/home/hulk1/data/periocular/hk/stats/vis_std.txt',
                         help='Path to file containing channel-wise image statistic')
-    parser.add_argument('--backbone', default='resnet18', type=str,
+    parser.add_argument('--backbone', type=str,
                         help='resnet18, resnet34, resnet50,'
                              'and their wider variants, resnet50x4')
-    parser.add_argument('--feat_dim', default=128, type=int,
+    parser.add_argument('--feat_dim', type=int,
                         help='feature dimension for contrastive loss')
     return parser.parse_args()
 
